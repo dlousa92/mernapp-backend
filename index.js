67d54bf87834eea5dce8d95284ff3a4b291f7b2b
Daniel Lousa <dlousa92@gmail.com>
2018-07-02 15:10:54 -0400

    connect index.js to model

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const parser = require('body-parser')
-const mongoose = require('')
+const mongoose = require('./db/schema')
 const cors = require('cors')
 
 const app = express()
@@ -8,11 +8,10 @@ const app = express()
 app.use(parser.json())
 app.use(cors())
 
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log('Ready for lift off on port 8000')
 })
