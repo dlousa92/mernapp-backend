e4502fc36dafe6a98617b0ffe33023987fc68203
Daniel Lousa <dlousa92@gmail.com>
2018-07-02 15:42:12 -0400

    have index js return list of superhero data

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const express = require('express')
 const parser = require('body-parser')
 const mongoose = require('./db/schema')
 const cors = require('cors')
+const Hero = mongoose.model('Hero')
 
 const app = express()
 
 app.use(parser.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api/heroes', (req, res) => {
+  Hero.find()
+    .then((heroes) => {
+      res.json(heroes)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 })
 
 app.listen(8000, () => {
