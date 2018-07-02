7232611f0d4e45d9c013a978133e31c2d128b712
Daniel Lousa <dlousa92@gmail.com>
2018-07-02 15:01:25 -0400

    import all technologies will use in index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const parser = require('body-parser')
+const mongoose = require('')
+const cors = require('cors')
+
 const app = express()
+
+app.use(parser.json())
+app.use(cors())
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
